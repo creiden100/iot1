f370f76715125d60c77d1e724dc60625ecd53f8e
sergio <nikolas_orozco@hotmail.com>
2018-06-01 13:15:01 -0500

    Initial commit

---
+++ App1/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using Android.OS;
+using App1.Views;
+using App1.Models;
+using Android.Graphics;
+
+namespace App1
+
+{
+    [Activity(Label = "Universidad Sergio Arboleda", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            var webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.Settings.JavaScriptEnabled = true;
+            webView.SetWebViewClient(new HybridWebViewClient());
+            // Render the view from the type generated from RazorView.cshtml  
+            webView.LoadUrl("https://liftlift.000webhostapp.com");
+
+        }
+        public class HybridWebViewClient : WebViewClient
+        {
+            public override bool ShouldOverrideUrlLoading(WebView view, string url)
+            {
+
+                view.LoadUrl(url);
+                return true;
+            }
+            public override void OnPageStarted(WebView view, string url, Android.Graphics.Bitmap favicon)
+            {
+                base.OnPageStarted(view, url, favicon);
+            }
+            public override void OnPageFinished(WebView view, string url)
+            {
+                base.OnPageFinished(view, url);
+            }
+            public override void OnReceivedError(WebView view, [GeneratedEnum] ClientError errorCode, string description, string failingUrl)
+            {
+                base.OnReceivedError(view, errorCode, description, failingUrl);
+            }
+
+
+
+
+        }
+    }
+
+}
